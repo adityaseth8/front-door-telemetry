196842db851425f906216f3cd05177a3af410f4f
adityaseth8 <aseth@ucdavis.edu>
2026-04-15 20:41:14 -0500

    batch

--- src/fastapi/write_to_fast_api.c
+++ src/fastapi/write_to_fast_api.c
@@ -8,6 +8,13 @@
 
 // #include <curl/curl.h>
 
+#define BATCH_SIZE 200  // 200 samples = ~10ms of data
+#define JSON_SIZE 128
+
+static char batch[BATCH_SIZE][JSON_SIZE];
+static int batch_idx = 0;
+
+
 struct SensorData {
     float x;
     float y;
@@ -21,21 +28,33 @@ int build_json(float x, float y, float z, char *json, size_t json_size, char *ti
                     x, y, z, timestamp);
 }
 
+void send_data(const char *json) {
+    // Placeholder - replace with actual HTTP POST code
+    // batch send - store in buffer until we have enough, then send as one big JSON array
+
+    strncpy(batch[batch_idx], json, JSON_SIZE - 1);
+    batch[batch_idx][JSON_SIZE - 1] = '\0';
+    batch_idx++;
+    // batch[batch_idx++] = json;
+    if (batch_idx >= BATCH_SIZE) {
+        // send_batch_http(batch, BATCH_SIZE);
+        printf("!!!Sending batch: \n");
+        for (int i = 0; i < BATCH_SIZE; i++) {
+            printf("  %s\n", batch[i]);
+        }
+        printf("\n\n");
+
+        batch_idx = 0;
+    }
+}
+
 void prepare_and_send_data(float x, float y, float z, char *timestamp) {
 
     // Build JSON string
     char json[128];
     build_json(x, y, z, json, sizeof(json), timestamp);
-    printf("JSON: %s\n", json);
-
-
-
-    
-    // batch[batch_idx++] = (Sample){x, y, z, esp_timer_get_time()};
-    // if (batch_idx >= BATCH_SIZE) {
-    //     send_batch_http(batch, BATCH_SIZE);
-    //     batch_idx = 0;
-    // }
+    // printf("JSON: %s\n", json);
+    send_data(json);
 }
 
 void format_timestamp(char *out, size_t out_size) {
@@ -62,32 +81,14 @@ int main() {
 
     while (true) {
         struct SensorData d = receive_sensor_data();
-        printf("Received - x: %f\ty: %f\tz: %f\n", d.x, d.y, d.z);
+        // printf("Received - x: %f\ty: %f\tz: %f\n", d.x, d.y, d.z);
         prepare_and_send_data(d.x, d.y, d.z, d.timestamp_us);
         
-        usleep(250000); // 25 ms
+        // usleep(250000); // 25 ms
 
     }
     return 0;
 }
-
-
-
-
-// #define BATCH_SIZE 200  // 200 samples = ~10ms of data
-
-// typedef struct { float x, y, z; uint64_t timestamp_us; } Sample;
-// static Sample batch[BATCH_SIZE];
-// static int batch_idx = 0;
-
-// void on_new_sample(float x, float y, float z) {
-//     batch[batch_idx++] = (Sample){x, y, z, esp_timer_get_time()};
-//     if (batch_idx >= BATCH_SIZE) {
-//         send_batch_http(batch, BATCH_SIZE);
-//         batch_idx = 0;
-//     }
-// }
-
 
 // int main() {
     
