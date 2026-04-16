c6c355e711cdef4ed8fd61927472200bb0792a6e
adityaseth8 <aseth@ucdavis.edu>
2026-04-15 20:29:49 -0500

    add timestamp

--- src/fastapi/write_to_fast_api.c
+++ src/fastapi/write_to_fast_api.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <time.h>
 // #include <cstdint>
 
 // #include <curl/curl.h>
@@ -10,34 +12,47 @@ struct SensorData {
     float x;
     float y;
     float z;
+    char timestamp_us[64];
 };
 
-int build_json(float x, float y, float z, char *json, size_t json_size) {
+int build_json(float x, float y, float z, char *json, size_t json_size, char *timestamp) {
     return snprintf(json, json_size,
-                    "{\"x\": %.3f, \"y\": %.3f, \"z\": %.3f}",
-                    x, y, z);
+                    "{\"x\": %.3f, \"y\": %.3f, \"z\": %.3f, \"timestamp\": \"%s\"}",
+                    x, y, z, timestamp);
 }
 
-void prepare_and_send_data(float x, float y, float z) {
+void prepare_and_send_data(float x, float y, float z, char *timestamp) {
 
     // Build JSON string
     char json[128];
-    build_json(x, y, z, json, sizeof(json));
+    build_json(x, y, z, json, sizeof(json), timestamp);
     printf("JSON: %s\n", json);
 
-     // batch[batch_idx++] = (Sample){x, y, z, esp_timer_get_time()};
+
+
+    
+    // batch[batch_idx++] = (Sample){x, y, z, esp_timer_get_time()};
     // if (batch_idx >= BATCH_SIZE) {
     //     send_batch_http(batch, BATCH_SIZE);
     //     batch_idx = 0;
     // }
 }
 
+void format_timestamp(char *out, size_t out_size) {
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+
+    strftime(out, out_size, "%Y-%m-%d %H:%M:%S", t);
+}
+
 struct SensorData receive_sensor_data() {
     struct SensorData data;
     
+    format_timestamp(data.timestamp_us, sizeof(data.timestamp_us)); // Placeholder - replace with actual timestamp
     data.x = (float)(rand() % 100) / 100;
     data.y = (float)(rand() % 100) / 100;
     data.z = (float)(rand() % 100) / 100;
+    
 
     return data; // Placeholder - replace with actual sensor data
 }
@@ -48,7 +63,7 @@ int main() {
     while (true) {
         struct SensorData d = receive_sensor_data();
         printf("Received - x: %f\ty: %f\tz: %f\n", d.x, d.y, d.z);
-        prepare_and_send_data(d.x, d.y, d.z);
+        prepare_and_send_data(d.x, d.y, d.z, d.timestamp_us);
         
         usleep(250000); // 25 ms
 
